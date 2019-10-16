20f9e0f074099ff84fa893a59a7985d148d4669c
Eduar Apaza <educartoons@gmail.com>
2019-10-16 13:28:04 -0500

    Added Permissions View

---
+++ components/Permissions.js
@@ -0,0 +1,140 @@
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Error from "./Error";
+import Table from "./styles/Table";
+
+const possiblePermissions = [
+  "ADMIN",
+  "USER",
+  "ITEMCREATE",
+  "ITEMUPDATE",
+  "ITEMDELETE",
+  "PERMISSIONUPDATE"
+];
+
+const ALL_USERS_QUERY = gql`
+  query {
+    users {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`;
+
+const UPDATE_PERMISSIONS_MUTATION = gql`
+  mutation UPDATE_PERMISSIONS_MUTATION(
+    $permissions: [Permission]
+    $userId: ID!
+  ) {
+    updatePermissions(permissions: $permissions, userId: $userId) {
+      id
+      permissions
+      name
+      email
+    }
+  }
+`;
+
+const Permissions = props => (
+  <Query query={ALL_USERS_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <Error error={error} />;
+      return (
+        <div>
+          <div>
+            <h2>Manage Permissions</h2>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  {possiblePermissions.map(permission => (
+                    <th key={permission}>{permission}</th>
+                  ))}
+                  <th>👇🏼</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.users.map(user => (
+                  <UserPermissions user={user} key={user.id} />
+                ))}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      );
+    }}
+  </Query>
+);
+
+class UserPermissions extends React.Component {
+  state = {
+    permissions: this.props.user.permissions
+  };
+  handlePermissionChange = e => {
+    const checkbox = e.target;
+    let updatedPermissions = [...this.state.permissions];
+    if (checkbox.checked) {
+      // Add it
+      updatedPermissions.push(checkbox.value);
+    } else {
+      // Remove it
+      updatedPermissions = updatedPermissions.filter(
+        permission => permission !== checkbox.value
+      );
+    }
+    this.setState({ permissions: updatedPermissions });
+  };
+  render() {
+    const user = this.props.user;
+    return (
+      <Mutation
+        mutation={UPDATE_PERMISSIONS_MUTATION}
+        variables={{
+          permissions: this.state.permissions,
+          userId: this.props.user.id
+        }}
+      >
+        {(updatePermissions, { loading, error }) => (
+          <>
+            {error && (
+              <tr>
+                <Error error={error} />
+              </tr>
+            )}
+            <tr>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              {possiblePermissions.map(permission => (
+                <td key={permission}>
+                  <label htmlFor={`${user.id}-permission-${permission}`}>
+                    <input
+                      type="checkbox"
+                      checked={
+                        this.state.permissions.includes(permission)
+                          ? "checked"
+                          : ""
+                      }
+                      value={permission}
+                      onChange={this.handlePermissionChange}
+                    />
+                  </label>
+                </td>
+              ))}
+              <td>
+                <button disabled={loading} onClick={updatePermissions}>
+                  Updat{loading ? "ing" : "e"}
+                </button>
+              </td>
+            </tr>
+          </>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+export default Permissions;
